0b23e875a305aca0b2afcfca53507215c973a292
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-11 21:33:21 +0100

    Test with overwrite MaterialUI components

---
+++ src/components/CustomButton.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+
+export default styled(Button)`
+  && {
+    color: white;
+    background-color: blue;
+    height: 80px;
+  }
+`;
+
+// https://medium.com/sipios/use-styled-components-with-material-ui-react-e0759f9a15ce
