c3c679de7d026e71a08c425051b3022a84646ffd
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-11 21:34:39 +0100

    Add MaterialUI grid to WelcomePage

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
-
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
@@ -8,29 +8,67 @@ import Navbar from '../../components/Navbar';
 import handcraftedImage from '../../assets/images/handcrafted.jpeg';
 
 
-// import styled from 'styled-components';
-// import img from './img/bg.gif';
-
-// const Content = styled.div`
-//     border: 1px solid #000;
-//     background-image: ;
-//     width: 2000px;
-//     height: 2000px;
-
 const Container = styled.div`
-  /* margin: ${({ theme }) => theme.padding.medium}; */
   background-image: url(${handcraftedImage});
   background-position: center;
   background-size: cover;
   width: 100%;
   height: 100vh;
+  overflow: hidden;
 `
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
 function WelcomePage() {
+  const classes = useStyles();
+
   return (
     <Container>
       <Navbar />
-
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+        </Grid>
+      </div>
     </Container>
   )
 }
