cf9b23b2336d05a565146f7c7e5c99a1c519e346
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-11 21:36:02 +0100

    Injecting themes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+
 import mainTheme from './themes/mainTheme';
+import theme from './themes/theme';
 
 import WelcomePage from './pages/WelcomePage/WelcomePage'
 
 function App() {
   return (
-    <ThemeProvider theme={mainTheme}>
-      <div className="App">
-        <WelcomePage />
-      </div>
-    </ThemeProvider>
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={mainTheme}>
+        <div className="App">
+          <WelcomePage />
+        </div>
+      </ThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
