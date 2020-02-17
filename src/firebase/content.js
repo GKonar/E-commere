151373898114b437f92e4e2c8b3241a7576eb0f7
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-17 21:03:51 +0100

    Create some db content [test]

---
+++ src/firebase/content.js
@@ -0,0 +1,107 @@
+export const products = [
+  {
+    name: 'Handmade basket',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '25$'
+  },
+  {
+    name: 'Colorful beads',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '50$'
+  },
+  {
+    name: 'Wooden bee',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '15$'
+  },
+  {
+    name: 'Wooden cat',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '15$'
+  },
+  {
+    name: 'Wooden cow',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '17$'
+  },
+  {
+    name: 'Wooden crock',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '19$'
+  },
+  {
+    name: 'Wooden dinosaur',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '21$'
+  },
+  {
+    name: 'Wooden dinosaurs beginner set',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '12$'
+  },
+  {
+    name: 'Wooden dinosaurs advanced set',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '10$'
+  },
+  {
+    name: 'Wooden elephant',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '43$'
+  },
+  {
+    name: 'Beautiful wooden fish',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '26$'
+  },
+  {
+    name: 'Wooden ladybird',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '12$'
+  },
+  {
+    name: 'Hand painted mask',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '17$'
+  },
+  {
+    name: 'Smiling octopus',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '18$'
+  },
+  {
+    name: 'Custom purse',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '112$'
+  },
+  {
+    name: 'Scandinavian Sweater',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '212$'
+  },
+  {
+    name: 'Handmade figure',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '46$'
+  },
+  {
+    name: 'Leather wallet',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '123$'
+  },
+  {
+    name: 'Happy Whale',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '48$'
+  },
+  {
+    name: 'Wooden shelves',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '160$'
+  },
+  {
+    name: 'Zoo set',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum laborum odio tempora eveniet quasi dolore placeat dolorum amet dicta accusantium, fuga ex! Molestiae quae voluptate fugiat delectus, deserunt nobis eligendi.',
+    price: '55$'
+  }
+]
