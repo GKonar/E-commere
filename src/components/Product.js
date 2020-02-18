ebe8a826f4a102cc108df91adec65a7c9bdd5dcf
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-18 15:15:25 +0100

    Create Product Component

---
+++ src/components/Product.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const ProductContainer = styled.div`
+  padding: 1rem;
+  padding-top: 2rem;
+  position: relative;
+  height: 100%; 
+
+  /*  Place image in the middle   */
+  display: flex;
+  justify-content: center;
+  align-items: center;    
+
+  &:hover .product-name {
+    transform: translateX(0);
+  }
+`
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+const SmallDescritpion = styled.span`
+  position: absolute;
+  overflow: hidden;
+`
+
+const Price = styled.p`
+  margin: 1rem;
+  margin-bottom: 0;
+  display: inline-flex;
+  background-color: ${({ theme }) => theme.color.secondary};
+  padding: .8rem;
+`
+
+const ProductName = styled.p`
+  position: relative;
+  top: -10px;
+  background-color: ${({ theme }) => theme.color.secondary};
+  padding: .8rem;
+  margin: 1rem;
+  margin-top: 0;
+  transition: .3s;
+  transform: translateX(-200px);
+`
+
+const Image = styled.img`
+  width: 100%;
+`
+
+function ProductPage({ price, name, image }) {
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <SmallDescritpion>
+          <Price>{price}</Price>
+          <ProductName className="product-name">{name}</ProductName>
+        </SmallDescritpion>
+        <Image src={image} alt="my-img" />
+      </ImageContainer>
+    </ProductContainer>
+  )
+}
+
+export default ProductPage
