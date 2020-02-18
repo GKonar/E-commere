5ad1132c842d8ead85d76a95556a59e9523b0b03
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-18 15:12:23 +0100

    Add test content for development

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import { Switch, Route, Link } from "react-router-dom";
 
 import mainTheme from './themes/mainTheme';
 import theme from './themes/theme';
 
-import WelcomePage from './pages/WelcomePage/WelcomePage'
-import ProductPage from './pages/ProductPage/ProductPage'
+
+
+import Routes from './Routes';
+
+//DEV
+import { products } from './testContent';
 
 function App() {
   return (
@@ -15,11 +18,7 @@ function App() {
       <MuiThemeProvider theme={theme}>
         <ThemeProvider theme={mainTheme}>
           <div className="App">
-
-            <Switch>
-              <Route exact path="/" component={WelcomePage} />
-              <Route exact path="/product" component={ProductPage} />
-            </Switch>
+            <Routes products={products} />
           </div>
         </ThemeProvider>
       </MuiThemeProvider>
---
+++ src/testContent.js
@@ -0,0 +1,47 @@
+import beads from './assets/images/beads.jpeg';
+import mask from './assets/images/mask.jpeg';
+import purse from './assets/images/purse.jpeg';
+// import sweater from './assets/images/sweater.jpeg';
+import woodenshelves from './assets/images/woodenshelves.jpeg';
+// import trumpet_man from './assets/images/trumpet_man.jpeg';
+// import octopus_toys from './assets/images/octopus_toys.jpeg';
+// import wallet from './assets/images/wallet.jpeg';
+// import zootoys from './assets/images/zootoys.jpeg';
+
+export const products = [
+  {
+    id: '1',
+    name: 'Colorful beds',
+    price: '30$',
+    images: [beads],
+    description: ' Quis iure eligendi ab, Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla adipisci quos sit molestias, laborum beatae tempore a aspernatur quo laboriosam velit amet, ipsam vitae hic impedit!',
+    inStock: true
+  },
+  {
+    id: '2',
+    name: 'Custom masks',
+    price: '35$',
+    images: [mask],
+    description: 'Ipsum dolor sit amet consectetur adipisicing elit. Quis iure eligendi ab, nulla adipisci quos sit molestias, laborum beatae tempore a aspernatur quo laboriosam velit amet, ipsam vitae hic impedit!',
+    inStock: true
+  },
+  {
+    id: '3',
+    name: 'Maroccan purse',
+    price: '65$',
+    images: [purse],
+    description: 'Dolor sit amet consectetur adipisicing elit. Quis iure eligendi ab, nulla adipisci quos sit molestias, laborum beatae tempore a aspernatur quo laboriosam velit amet, ipsam vitae hic impedit!',
+    inStock: true
+  },
+  {
+    id: '4',
+    name: 'Wooden shelves',
+    price: '110$',
+    images: [woodenshelves],
+    description: 'Lorem ipsum dolor sit amet elit. Quis iure eligendi ab, nulla adipisci quos sit molestias, laborum beatae tempore a aspernatur quo laboriosam velit amet, ipsam vitae hic impedit!',
+    inStock: true
+  },
+]
+
+// inStock: true/false
+// 
