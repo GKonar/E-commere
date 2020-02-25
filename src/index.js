b5a71ddc0bb1bd758816b0026b981cfaab96b256
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-25 12:44:42 +0100

    Import thunk, add redux devtools , create custom logger middleware

--- src/index.js
+++ src/index.js
@@ -6,12 +6,27 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 // REDUX
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './store/reducer';
-const store = createStore(reducer);
+import reducer from './store/reducers/reducer';
+import thunk from 'redux-thunk';
 
-console.log('REDUX STATE: ', store.getState());
+// Custom middleware
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching ', action);
+      const result = next(action);
+      console.log('[Middleware] next state ', store.getState())
+      return result;
+    }
+  }
+};
+
+// React Devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(
   <BrowserRouter>
