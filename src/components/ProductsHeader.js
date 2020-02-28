a9b407441d5db4b6d5c17d4dcab519df4dbfccf4
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-28 20:13:47 +0100

    Extract ProductHeader to separate component

---
+++ src/components/ProductsHeader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import H1 from './H1';
+
+const Container = styled(H1)`
+  && {
+    margin-bottom: 0;
+    margin-top: ${({ theme }) => theme.margin.medium};
+    justify-content: center;
+    position: relative;
+  }
+  /* &::after {
+      content: "";
+      display: flex;
+      background-color: ${({ theme }) => theme.color.main};
+      width: 25%;
+      height: 2px;
+      position: absolute;
+      top: 110%;
+      z-index: 1;
+      transition: all .1s;
+      text-align: center;
+    } */
+`
+
+function ProductsHeader({ children }) {
+  return (
+    <Container>{children}</Container>
+  )
+}
+
+
+export default ProductsHeader
