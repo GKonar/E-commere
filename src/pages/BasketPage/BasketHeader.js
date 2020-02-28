e91a71ce0ff7ff8f9eb919d198b39450be03c727
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-02-28 20:12:37 +0100

    Extract navigation buttons to separate components

--- src/pages/BasketPage/BasketHeader.js
+++ src/pages/BasketPage/BasketHeader.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import MoneyOffIcon from '@material-ui/icons/MoneyOff';
 
-import CustomButton from '../../components/CustomButton';
 import H1 from '../../components/H1';
+import NavigationButtons from './NavigationButtons';
+
 
 const Container = styled.div`
   display: flex;
@@ -30,30 +30,6 @@ const FreeDeliveryCount = styled.div`
   transform: rotate(2deg);
 `
 
-const NavigationButtons = styled.div`
-  width: 40%;
-  display: flex;
-`
-
-const ContinueShoppingButton = styled(CustomButton)`
-  && {
-    margin: 0 2rem;
-  }
-`
-
-const CheckoutButton = styled(CustomButton)`
-  && {
-    color: ${({ theme }) => theme.textColor.secondary};
-    background-color: ${({ theme }) => theme.color.primary};
-    border: 1px solid #2f3542;
-    margin: 0 2rem;
-    &:hover {
-      background-color: ${({ theme }) => theme.color.white};
-      color: ${({ theme }) => theme.textColor.primary};
-    }
-  }
-`
-
 const Heading = styled(H1)`
   && {
     margin: 0;
@@ -75,10 +51,7 @@ function BasketHeader({ toFreeDelivery }) {
               <FreeDeliveryCount>You get free shipping !</FreeDeliveryCount>
             )}
         </HeaderLeft>
-        <NavigationButtons>
-          <ContinueShoppingButton><Link to='/'>Continue Shopping</Link></ContinueShoppingButton>
-          <CheckoutButton>Checkout Now</CheckoutButton>
-        </NavigationButtons>
+        <NavigationButtons />
       </Container>
     </div>
   )
---
+++ src/pages/BasketPage/NavigationButtons.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+import CustomButton from '../../components/CustomButton';
+
+const Container = styled.div`
+  width: 40%;
+  display: flex;
+`
+
+const ContinueShoppingButton = styled(CustomButton)`
+  && {
+    margin: 0 2rem;
+  }
+`
+
+const CheckoutButton = styled(CustomButton)`
+  && {
+    color: ${({ theme }) => theme.textColor.secondary};
+    background-color: ${({ theme }) => theme.color.primary};
+    border: 1px solid #2f3542;
+    margin: 0 2rem;
+    &:hover {
+      background-color: ${({ theme }) => theme.color.white};
+      color: ${({ theme }) => theme.textColor.primary};
+    }
+  }
+`
+
+function NavigationButtons() {
+  return (
+    <Container>
+      <ContinueShoppingButton><Link to='/'>Continue Shopping</Link></ContinueShoppingButton>
+      <CheckoutButton>Checkout Now</CheckoutButton>
+    </Container>
+  )
+}
+
+export default NavigationButtons
