bca2c36b72fc767e6bb5823cb21eba2453755f20
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-03-04 21:36:44 +0100

    Install stripe dependencies to start testing handling payments

--- src/App.js
+++ src/App.js
@@ -9,8 +9,16 @@ import theme from './themes/theme';
 import Routes from './Routes';
 import Navbar from './components/Navbar';
 
+// STRIPE payments
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
 // REDUX
 import { connect } from 'react-redux';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid // from DOCS
+// recreating the `Stripe` object on every render. 
+const stripePromise = loadStripe("pk_test_odEMo2bB5CVGgwWwclSUfqWO00SsiPJxQU");
 
 function App({ forHer, forHim, forHome, toys, hottest, newest }) {
   const shopItems = {
@@ -24,14 +32,16 @@ function App({ forHer, forHim, forHome, toys, hottest, newest }) {
 
   return (
     <div>
-      <MuiThemeProvider theme={theme}>
-        <ThemeProvider theme={mainTheme}>
-          <Navbar />
-          <div className="App" style={{ marginTop: '80px' }}>
-            <Routes products={shopItems} />
-          </div>
-        </ThemeProvider>
-      </MuiThemeProvider>
+      <Elements stripe={stripePromise}>
+        <MuiThemeProvider theme={theme}>
+          <ThemeProvider theme={mainTheme}>
+            <Navbar />
+            <div className="App" style={{ marginTop: '80px' }}>
+              <Routes products={shopItems} />
+            </div>
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </Elements>
     </div>
   );
 }
