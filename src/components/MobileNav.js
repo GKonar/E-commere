dae460af06168f2381296802bc29adf467fe0f98
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-03-14 20:46:39 +0100

    MobileNav ready to implement

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,31 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Divider from '@material-ui/core/Divider';
+
+import Icon from '../components/Icon';
+
+import styled from 'styled-components';
+
+import hipSquare from '../assets/images/hip-square.png';
 
 const useStyles = makeStyles({
   list: {
     width: 250,
+    height: '100%',
+    backgroundImage: `url(${hipSquare})`,
+    "& a": {
+      textDecoration: 'none',
+      color: 'inherit'
+    }
   },
   fullList: {
     width: 'auto',
   },
 });
 
+const Logo = styled.span`
+  font-weight: 600;
+  margin-left: ${({ theme }) => theme.margin.medium};
+
+  a {
+    display:flex; 
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
+  }
+`
+
+const LogoText = styled.span`
+  margin-right: 2px;
+  letter-spacing: 1px;
+`
+
 function MobileNav() {
   const classes = useStyles();
   const [state, setState] = React.useState({
-    top: false,
     left: false,
-    bottom: false,
-    right: false,
   });
 
   const toggleDrawer = (side, open) => event => {
@@ -44,28 +70,56 @@ function MobileNav() {
       onKeyDown={toggleDrawer(side, false)}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
+        <Logo>
+          <Link to='/'>
+            <LogoText>hand</LogoText>
+            <Icon icon={'WOOL'} color='#F59486' size={30} />
+            <LogoText>crafted</LogoText>
+          </Link>
+        </Logo>
+        <br />
+        <Link to="/for-him">
+          <ListItem button key={'ForHim'}>
+            <ListItemText primary={'For Him'} />
           </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
+        </Link>
+        <Link to="/for-her">
+          <ListItem button key={'ForHer'}>
+            <ListItemText primary={'For Her'} />
           </ListItem>
-        ))}
+        </Link>
+        <Link to="/for-home">
+          <ListItem button key={'ForHome'}>
+            <ListItemText primary={'For Home'} />
+          </ListItem>
+        </Link>
+        <Link to="/toys">
+          <ListItem button key={'Toys'}>
+            <ListItemText primary={'Toys'} />
+          </ListItem>
+        </Link>
+        <Link to="/shop">
+          <ListItem button key={'Shop'}>
+            <ListItemText primary={'Shop'} />
+          </ListItem>
+        </Link>
+        <Link to="/contact">
+          <ListItem button key={'Contact'}>
+            <ListItemText primary={'Contact'} />
+          </ListItem>
+        </Link>
+        <Link to="/about">
+          <ListItem button key={'About'}>
+            <ListItemText primary={'About'} />
+          </ListItem>
+        </Link>
       </List>
     </div>
   );
 
   return (
     <div>
-      <Button onClick={toggleDrawer('left', true)}>Open Left</Button>
+      <Button onClick={toggleDrawer('left', true)}>Nav</Button>
       <SwipeableDrawer
         open={state.left}
         onClose={toggleDrawer('left', false)}
@@ -78,3 +132,5 @@ function MobileNav() {
 }
 
 export default MobileNav;
+
+            // <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
