ea81e9f78998188040c99dffcc5ad1998a0f8c14
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-03-16 23:27:22 +0100

    Simplify BigDescription, add responsive design

--- src/components/BigDescription.js
+++ src/components/BigDescription.js
@@ -1,30 +1,49 @@
 import React from 'react'
 import styled from 'styled-components';
 
+import { sizes } from '../helpers/sizes';
+
 const Description = styled.div`
   position: absolute;
   color: ${({ theme }) => theme.textColor.secondary};
   padding: 2rem;
   background-color: rgba(0, 0, 0, 0.2);
+  top: 1rem;
+  left: 1rem;
+
+  ${sizes.laptopM} {
+    padding: 1.5rem;
+    top: 10px;
+    left: 10px;
+  }
+  
 `
 const DescriptionHeadaer = styled.h1`
   font-size: ${({ theme }) => theme.textSize.huge};
   font-weight: 700;
   margin: 0;
+
+  ${sizes.laptopM} {
+    font-size: ${({ theme }) => theme.textSize.bigger};
+  }
+  
+  ${sizes.mobileL} {
+    font-size: ${({ theme }) => theme.textSize.big};
+  }
 `
 const DescriptionParagraph = styled.p`
   font-size: ${({ theme }) => theme.textSize.readable};
   font-weight: 600;
   margin: 0;
+
+  ${sizes.laptopM} {
+    font-size: ${({ theme }) => theme.textSize.secondary};
+  }
 `
 
 function BigDescription({
   header,
   description,
-  top = '5%',
-  right = '100px',
-  left,
-  bottom,
   background,
   setBackground
 }) {
@@ -34,7 +53,7 @@ function BigDescription({
 
   return (
     <Description
-      style={{ top, right, left, bottom, background }}>
+      style={{ background }}>
       <DescriptionHeadaer>{header}</DescriptionHeadaer>
       <DescriptionParagraph>{description}</DescriptionParagraph>
     </Description>
