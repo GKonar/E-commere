498b637e2ac12effadc677c87d633386432fae6a
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-03-20 20:21:35 +0100

    Create authStart, authSuccess, authFail action creators and async auth, handle firebase login and signup

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,14 +1,17 @@
-import axios from '../../axios';
+import { instance } from '../../axios';
+import axios from 'axios';
 
 // actions identifires 
 export const FETCH_PAGE_ITEMS = 'FETCH_PAGE_ITEMS';
 export const ADD_ITEM = 'ADD_ITEM';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
-export const UPDATE_BASKET = 'UPDATE_BASKET';
 export const INCREMENT_ITEM_QUANTITY = 'INCREMENT_ITEM_QUANTITY';
 export const DECREMENT_ITEM_QUANTITY = 'DECREMENT_ITEM_QUANTITY';
 export const SET_DISCOUNT = 'SET_DISCOUNT';
 export const CLEAR_BASKET = 'CLEAR_BASKET';
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
 
 // sync action creators 
 const savePageItems = (data, page) => {
@@ -54,23 +57,63 @@ export const setDiscount = (basketValue) => {
   }
 }
 
-export const updateBasket = (item) => {
-  return {
-    type: UPDATE_BASKET,
-    item
-  }
-}
-
 export const clearBasket = () => {
   return {
     type: CLEAR_BASKET,
   }
 }
 
+export const authStart = () => {
+  return {
+    type: AUTH_START
+  }
+}
+
+export const authSuccess = (idToken, userId) => {
+  return {
+    type: AUTH_SUCCESS,
+    idToken,
+    userId
+  }
+}
+
+export const authFail = (error) => {
+  return {
+    type: AUTH_FAIL,
+    error
+  }
+}
+
 // async action creators
+export const auth = (email, password, mode) => { // mode - singup or login
+  // console.log(email, password, mode) // DEV
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    }
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAnecAp8bG-KwhlHm_a0AaQFaGLkhiZakc';
+    if (mode === 'login') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAnecAp8bG-KwhlHm_a0AaQFaGLkhiZakc';
+    }
+    axios.post(url, authData)
+      .then((response) => {
+        // console.log(response) // DEV
+        dispatch(authSuccess(response.data.idToken, response.data.localId))
+      })
+      .catch((err) => {
+        // console.log(err.response.data.error.message); // DEV
+        // console.log(err.response); // DEV
+        dispatch(authFail(err.response.data.error));
+      })
+  }
+}
+
 export const fetchPageItems = (page) => {
   return dispatch => {
-    axios.get(`/products/${page}.json`)
+    instance.get(`/products/${page}.json`)
       .then(res => {
         let pageProducts = res.data;
         pageProducts = Object.entries(pageProducts);
