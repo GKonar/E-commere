85ba5150369733459fb800a98ff630d803eae499
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-03-28 22:28:18 +0100

    Add Login component

---
+++ src/components/Navbar/Login.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ReactComponent as LoginIcon } from '../../assets/svg/login.svg';
+
+// REDUX
+import { connect } from 'react-redux';
+
+// SVG Components
+import styled from 'styled-components';
+
+const IconWrapper = styled.div`
+  width: 35px;
+  height: 35px;
+  margin: ${({ theme }) => theme.margin.small};
+  transition: .2s;
+  border-radius: 50%;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+function Login({ loggedIn }) {
+  console.log('LOGGED IN: ', loggedIn);
+  return (
+    <IconWrapper style={{ backgroundColor: loggedIn ? "#32ff7e" : "#FFF" }}>
+      <LoginIcon />
+    </IconWrapper>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(Login);
